ddbaf21537d6653884eca6ddc1c38618bfe4aa17
David Welch <david.welch@vml.com>
2018-04-05 12:44:22 -0500

    Updating index.js to match original

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import Slide from "./Slide.vue";
 import MultiRowSlide from "./MultiRowSlide.vue";
 import Thumbnails from "./ThumbnailNav.vue";
 
-// const install = Vue => {
-//   Vue.component("carousel", Carousel);
-//   Vue.component("slide", Slide);
-//   Vue.component("thumbnails", Thumbnails);
-// };
-//
-// export default {
-//   install
-// };
+const install = Vue => {
+  Vue.component("carousel", Carousel);
+  Vue.component("slide", Slide);
+  Vue.component("multi-row-slide", MultiRowSlide);
+  Vue.component("thumbnails", Thumbnails);
+};
 
-Vue.component("carousel", Carousel);
-Vue.component("slide", Slide);
-Vue.component("multi-row-slide", MultiRowSlide);
-Vue.component("thumbnails", Thumbnails);
-
-export { Carousel, Slide, Thumbnails };
+export default {
+  install
+};
